86050e34ef20f37d7e598b36df8a54160bac5b1a
NahuelLopez18 <nahuel@tots.agency>
2023-09-28 15:25:05 -0300

    The product controller tests were modified to return json and the corresponding status.

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,37 +1,45 @@
 require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
+
   test "should create product with valid price on day 5" do
     travel_to Time.zone.local(2023, 9, 5, 12, 0, 0) do
-      post products_url, params: { product: { name: "Test Product", description: "Test Description", price: 4000 } }
-    end
-    assert_redirected_to product_path(assigns(:product))
-    assert_equal "Producto creado exitosamente.", flash[:notice]
-  end
-
-  test "should create product with valid price on day 20" do
-    travel_to Time.zone.local(2023, 9, 20, 12, 0, 0) do
-      post products_url, params: { product: { name: "Test Product", description: "Test Description", price: 6000 } }
-    end
-    assert_redirected_to product_path(assigns(:product))
-    assert_equal "Producto creado exitosamente.", flash[:notice]
-  end
-
-  test "should not create product with invalid price on day 5" do
-    travel_to Time.zone.local(2023, 9, 5, 12, 0, 0) do
-      post products_url, params: { product: { name: "Test Product", description: "Test Description", price: 6000 } }
+      post products_url, params: { product: { name: "Test Product", description: "Test Description", price: 4000 } }, headers: { 'Accept' => 'application/json' }
     end
     assert_response :success
-    assert_template :new
-    assert_includes assigns(:product).errors[:price], "Debe ser menor o igual a 5000 en los primeros 15 días del mes"
+    response_body = JSON.parse(response.body)
+    puts "Success: #{response_body}"
   end
+  
+  test "should create product with valid price on day 20" do
+    travel_to Time.zone.local(2023, 9, 20, 12, 0, 0) do
+      post products_url, params: { product: { name: "Test Product", description: "Test Description", price: 6000 } }, headers: { 'Accept' => 'application/json' }
+    end
+    assert_response :success
+    response_body = JSON.parse(response.body)
+    puts "Success: #{response_body}"
+  end
+  
+  
+  test "should not create product with invalid price on day 5" do
+    travel_to Time.zone.local(2023, 9, 5, 12, 0, 0) do
+      post products_url, params: { product: { name: "Test Product", description: "Test Description", price: 6000 } }, headers: { 'Accept' => 'application/json' }
+    end
+    assert_response :unprocessable_entity
+    response_json = JSON.parse(response.body)
+    response_json.each do |field, errors|
+      puts "Error #{field}: #{errors.join(', ')}" if errors.present?
+    end    
+  end  
 
   test "should not create product with invalid price on day 20" do
     travel_to Time.zone.local(2023, 9, 20, 12, 0, 0) do
-      post products_url, params: { product: { name: "Test Product", description: "Test Description", price: 4000 } }
+      post products_url, params: { product: { name: "Test Product", description: "Test Description", price: 4000 } }, headers: { 'Accept' => 'application/json' }
     end
-    assert_response :success
-    assert_template :new
-    assert_includes assigns(:product).errors[:price], "Debe ser mayor a 5000 después del día 15 del mes"
+    assert_response :unprocessable_entity
+    response_json = JSON.parse(response.body)
+    response_json.each do |field, errors|
+      puts "Error #{field}: #{errors.join(', ')}" if errors.present?
+    end    
   end
 end
